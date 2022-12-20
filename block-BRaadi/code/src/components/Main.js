b6d9725d27874822ddf7d1c784e95cf05a884fe4
somanshu63 <somanshu63@gmail.com>
2022-12-20 17:16:19 +0530

    use of error boundary

--- block-BRaadi/code/src/components/Main.js
+++ block-BRaadi/code/src/components/Main.js
@@ -1,3 +1,4 @@
+import ErrorBoundary from "./errorBoundary";
 import Location from "./Location";
 import SectionOne from "./SectionOne";
 import SectionTwo from "./SectionTwo";
@@ -6,9 +7,15 @@ function Main() {
   return (
     <section id="section-one">
       <div className="grid">
-        <SectionOne />
-        <SectionTwo />
-        <Location />
+        <ErrorBoundary message="something went wrong! please reload the page">
+          <SectionOne />
+        </ErrorBoundary>
+        <ErrorBoundary message="An error occured!">
+          <SectionTwo />
+        </ErrorBoundary>
+        <ErrorBoundary message="something went wrong! please reload the page as you can't pass empty data">
+          <Location />
+        </ErrorBoundary>
       </div>
     </section>
   );
